e73f2714bfa540340344cb85c13983cd32acb1f0
AndreyMamchur <mamchurandrey87@gmail.com>
2018-01-26 13:32:56 +0200

    Add org.bouncycastle to pom.xml. Add method getPrivateKeyFromString to EncryptionDecryptionXml class

--- src/main/java/com/paymentgameservice/service/EncryptionDecryptionXml.java
+++ src/main/java/com/paymentgameservice/service/EncryptionDecryptionXml.java
@@ -1,15 +1,19 @@
 package com.paymentgameservice.service;
 
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.ASN1Sequence;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.*;
 import java.security.spec.*;
 
+import java.util.Enumeration;
 import java.util.List;
 
 @Component
@@ -21,7 +25,7 @@ public class EncryptionDecryptionXml {
         try {
             Signature sign = Signature.getInstance("SHA1withRSA");
             String key = readFileToString(privateFile);
-            PrivateKey privateKey = privateKeyFromString(key);
+            PrivateKey privateKey = getPrivateKeyFromString(key);
             sign.initSign(privateKey);
             sign.update(message.getBytes("UTF-8"));
             return new String(Base64.encodeBase64(sign.sign()),"UTF-8");
@@ -34,7 +38,7 @@ public class EncryptionDecryptionXml {
         try {
             Signature sign = Signature.getInstance("SHA1withRSA");
             String key = readFileToString(publicFile);
-            PublicKey publicKey = publicKeyFromString(key);
+            PublicKey publicKey = getPublicKeyFromString(key);
             sign.initVerify(publicKey);
             sign.update(message.getBytes("UTF-8"));
             return sign.verify(Base64.decodeBase64(signature.getBytes("UTF-8")));
@@ -42,6 +46,7 @@ public class EncryptionDecryptionXml {
             throw new SignatureException(ex);
         }
     }
+
 
     public String readFileToString(String nameOfFile){
         String key="";
@@ -58,21 +63,7 @@ public class EncryptionDecryptionXml {
         return key;
     }
 
-    public PrivateKey privateKeyFromString(String key){
-        key = key.replaceAll("\\n", "").replace("-----BEGIN RSA PRIVATE KEY-----", "").replace("-----END RSA PRIVATE KEY-----", "");
-        System.out.println(key);
-        try {
-            byte[] privKeyBytes=Base64.decodeBase64(key);
-            KeyFactory keyFactory=KeyFactory.getInstance("RSA");
-            KeySpec privSpec=new PKCS8EncodedKeySpec(privKeyBytes);
-            return keyFactory.generatePrivate(privSpec);
-        }
-        catch (  Exception e) {
-            throw new IllegalStateException("Error loading private key: " + e);
-        }
-    }
-
-    public PublicKey publicKeyFromString(String key){
+    public PublicKey getPublicKeyFromString(String key){
         key = key.replaceAll("\\n", "").replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "");
         System.out.println(key);
         try {
@@ -86,4 +77,37 @@ public class EncryptionDecryptionXml {
         }
     }
 
+    public PrivateKey getPrivateKeyFromString(String privateKeyString){
+        String privKeyPEM = privateKeyString.replace(
+                "-----BEGIN RSA PRIVATE KEY-----\n", "")
+                .replace("-----END RSA PRIVATE KEY-----", "");
+
+        byte[] encodedPrivateKey = Base64.decodeBase64(privKeyPEM);
+
+        try {
+            ASN1Sequence primitive = (ASN1Sequence) ASN1Sequence
+                    .fromByteArray(encodedPrivateKey);
+            Enumeration<?> e = primitive.getObjects();
+            BigInteger v = ((ASN1Integer) e.nextElement()).getValue();
+
+            int version = v.intValue();
+            if (version != 0 && version != 1) {
+                throw new IllegalArgumentException("wrong version for RSA private key");
+            }
+
+            BigInteger modulus = ((ASN1Integer) e.nextElement()).getValue();
+            BigInteger privateExponent = ((ASN1Integer) e.nextElement()).getValue();
+
+            RSAPrivateKeySpec spec = new RSAPrivateKeySpec(modulus, privateExponent);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            PrivateKey pk = kf.generatePrivate(spec);
+            return pk;
+        } catch (IOException e2) {
+            throw new IllegalStateException();
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+        } catch (InvalidKeySpecException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
